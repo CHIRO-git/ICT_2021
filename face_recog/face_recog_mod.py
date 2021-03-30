0042747ec8e407f12a49d8d5ff8248f623b728ba
lopeace <73591643+lopeace@users.noreply.github.com>
2021-03-31 02:05:07 +0900

    정리

--- face_recog/face_recog_mod.py
+++ face_recog/face_recog_mod.py
@@ -4,24 +4,26 @@ import cv2
 import numpy as np
 import os
 
-cwd			= os.path.abspath(os.path.dirname(__file__))
+cwd = os.path.abspath(os.path.dirname(__file__))
 
-detector 	= dlib.get_frontal_face_detector()
-predictor 	= dlib.shape_predictor(os.path.join(cwd, "shape_predictor_68_face_landmarks.dat"))
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(os.path.join(cwd, "shape_predictor_68_face_landmarks.dat"))
 
 # range doesn't6 include last data
-ALL 			= list(range(0, 68))
-RIGHT_EYEBROW 	= list(range(17, 22))
-LEFT_EYEBROW 	= list(range(22, 27))
-RIGHT_EYE 		= list(range(36, 42))
-LEFT_EYE 		= list(range(42, 48))
-NOSE 			= list(range(27, 36))
-MOUTH_OUTLINE 	= list(range(48, 61))
-MOUTH_INNER 	= list(range(61, 68))
-JAWLINE 		= list(range(0, 17))
-FACE_COMPO 		= list(range(17, 68))
+ALL = list(range(0, 68))
+RIGHT_EYEBROW = list(range(17, 22))
+LEFT_EYEBROW = list(range(22, 27))
+RIGHT_EYE = list(range(36, 42))
+LEFT_EYE = list(range(42, 48))
+NOSE = list(range(27, 36))
+MOUTH_OUTLINE = list(range(48, 61))
+MOUTH_INNER = list(range(61, 68))
+JAWLINE = list(range(0, 17))
+FACE_COMPO = list(range(17, 68))
 
-index 			= ALL
+index = ALL
+
+
 
 
 
@@ -36,8 +38,22 @@ def activate(vid):
         ret, frame = vid.read()
 
         if ret:
-            cv2.imshow('frame_color', frame)
-            if cv2.waitKey(1) == ord('c'):  # if button input True, capture and save image as jpg
+            # img_frame = cv2.flip(img_frame, 1)  # only use for test! mirror mode
+            img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            dets = detector(img_gray, 1)
+            Detected = False
+            for face in dets:
+                Detected = True
+                shape = predictor(frame, face)  # detect 68 dots from face
+
+                list_points = []
+                for p in shape.parts():
+                    list_points.append([p.x, p.y])
+
+                list_points = np.array(list_points)
+
+            if Detected:  # if button input True, capture and save image as jpg
                 cv2.imwrite(os.path.join(cwd, 'user/user_img.jpg'), frame)
                 break
 
@@ -60,23 +76,23 @@ def detect(vid):
     encodings =[]
     imgpath = os.path.join(cwd, 'user/user_img.jpg')
     if os.path.isfile(imgpath):
-        image 		= face_recognition.load_image_file(imgpath)
-        encoding 	= face_recognition.face_encodings(image)[0]
+        image = face_recognition.load_image_file(imgpath)
+        encoding = face_recognition.face_encodings(image)[0]
         encodings.append(encoding)
     else:
         print('No Userdata')
         return False
-    count 			= 0
-    face_locations 	= []
-    face_encodings 	= []
+    count = 0
+    face_locations = []
+    face_encodings = []
     while True:
-        count 			+= 1
-        ret, frame 		= vid.read()
-        small_frame 	= cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        count += 1
+        ret, frame = vid.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
         rgb_small_frame = small_frame[:, :, ::-1]
 
-        face_locations 	= face_recognition.face_locations(rgb_small_frame)
-        face_encodings 	= face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
@@ -100,12 +116,12 @@ def head(img_frame):
     # img_frame = cv2.flip(img_frame, 1)  # only use for test! mirror mode
     img_gray = cv2.cvtColor(img_frame, cv2.COLOR_BGR2GRAY)
 
-    dets 		= detector(img_gray, 1)
-    Detected 	= False
-    concent 	= False
+    dets = detector(img_gray, 1)
+    Detected = False
+    concent = False
     for face in dets:
-        Detected 	= True
-        shape 		= predictor(img_frame, face)  # detect 68 dots from face
+        Detected = True
+        shape = predictor(img_frame, face)  # detect 68 dots from face
 
         list_points = []
         for p in shape.parts():
@@ -115,13 +131,13 @@ def head(img_frame):
 
     # detect turning head
     if Detected:
-        left_x 		= list_points[33][0] - list_points[31][0]
-        left_y 		= list_points[33][1] - list_points[31][1]
-        right_x 	= list_points[35][0] - list_points[33][0]
-        right_y 	= list_points[35][1] - list_points[33][1]
-        nose_left 	= np.sqrt(left_x * left_x + left_y * left_y)
-        nose_right 	= np.sqrt(right_x * right_x + right_y * right_y)
-        nose_ratio 	= nose_right / nose_left
+        left_x = list_points[33][0] - list_points[31][0]
+        left_y = list_points[33][1] - list_points[31][1]
+        right_x = list_points[35][0] - list_points[33][0]
+        right_y = list_points[35][1] - list_points[33][1]
+        nose_left = np.sqrt(left_x * left_x + left_y * left_y)
+        nose_right = np.sqrt(right_x * right_x + right_y * right_y)
+        nose_ratio = nose_right / nose_left
         if nose_ratio > 1.2:
             concent = False
             print("right")
@@ -130,7 +146,7 @@ def head(img_frame):
             print("left")
         else:
             concent = True
-            print("forward", end="\t")
+            print("forward")
     else :
         print("face missed!")
 
